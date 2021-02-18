a6db84cac2b202d3a85a7b5972a7e5c439c77963
kevin <kevin@192.168.10.101>
2021-02-18 14:28:47 +0100

    zonder representation exposure (met .clone())

--- intlist/src/intlist/tests/IntListTest.java
+++ intlist/src/intlist/tests/IntListTest.java
@@ -15,14 +15,19 @@ class IntListTest {
 		
 		int[] elements = intList.getElements();
 		assertEquals(42, elements[0]);
-		assertArrayEquals(new int[] {42}, elements);
+		//assertArrayEquals(new int[] {42}, elements);
 		
-		elements[0] = 43;
-		assertEquals(42, intList.getElementAt(0));
+		/*elements[0] = 43;
+		assertEquals(42, intList.getElementAt(0));*/
 		// kopie of niet?
 		// representation exposure: als het wijzigen van 
 		// iets teruggekregen met een get(), het origineel wijzigt
 		// als dit gebeurt dan klopt dit hier: assertEquals(43, intList.getElementAt(0));
+		
+		
+		intList.setElement(0, 43);
+		assertEquals(43, elements[0]); // als dit klopt -> REPRESENTATION EXPOSURE
+		// klant wijzigt het inList object en ziet dat ook de int array gewijzigd is!
 	}
 
 }
